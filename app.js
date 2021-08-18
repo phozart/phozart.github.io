b42c0b78796fea9bc927edc5ed3dd66e7cc454be
Peter Hardy <87307429+phozart@users.noreply.github.com>
2021-08-18 10:57:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function loadHeaderButtons()
 function createBall()
 {
     let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    divOutput.innerHTML = "";
+   
         ball = document.createElement("div");
         ball.style.backgroundColor = randomColor;
         ball.className = "ball";
@@ -107,7 +107,7 @@ function updateBalls() {
 
   function ballFactory()
   {
-      divOutput.innerHTML = "";
+     
     var nob = prompt("Please enter a number between 0 and 100 to create this amount of balls.", "Enter a number!");
    if (nob <= 100 && nob >=0 ){
     for (let i=0; i < nob; i++)
