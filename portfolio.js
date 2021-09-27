2f65f04ac18fbea8e8b2d7ffcdd1fb81a41869c7
Peter Hardy <peter@phozart.com>
2021-09-27 21:35:08 +0200

    updateContactForm

--- portfolio.js
+++ portfolio.js
@@ -55,6 +55,7 @@ function navigate(tab) {
         break;
     case "Contact":
         loadContact();
+        break;
     default:
         loadHome();
     }
@@ -145,10 +146,9 @@ function loadProjects() {
 function loadContact() {
     
     right.innerHTML = "";
-    right.innerHTML += "<div>Contact form come here</div";
     //following contact form is copied from https://mdbootstrap.com/docs/b4/jquery/forms/contact/#html
     right.innerHTML += '<section class="mb-4">'
-        + '<h2 class="h1-responsive font-weight-bold text-center my-4">Contact me</h2>'
+        + '<h2 class="h2-responsive font-weight-bold text-left my-4">Contact me</h2>'
         + '<div class="row"> <div class="col-md-9 mb-md-0 mb-5">'
         +  '<form id="contact-form" name="contact-form" action="mail.php" method="POST">'
         +  '<div class="row"><div class="col-md-6"><div class="md-form mb-0">'
@@ -164,11 +164,38 @@ function loadContact() {
         +  '<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>'
         +  '<label for="message">Your message</label> </div></div></div></form>'
         +  '<div class="text-center text-md-left">'
-        +  '<a class="btn btn-primary" onclick="document.getElementById(&apos;contact-form&apos;).submit();">Send</a>'
-        +  '</div><div class="status"></div> </div><div class="col-md-3 text-center">'
-        +  '<ul class="list-unstyled mb-0"><li><i class="fas fa-map-marker-alt fa-2x"></i><p>Belgium</p></li>'
-        +  '<li><i class="fas fa-phone mt-4 fa-2x"></i><p>+ 32 493 63 01 15</p></li>'
-        +  '<li><i class="fas fa-envelope mt-4 fa-2x"></i><p>peter.hardy@ouiquic.com</p></li></ul></div></div></section></div>'
+        +  '<a class="btn btn-primary" onclick="validateForm();">Send</a>'
+        +  '</div></div></section>';
 
 
 }
+
+function validateForm() {
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.querySelector('.status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.querySelector('.status').innerHTML = "Sending...";
+  }
