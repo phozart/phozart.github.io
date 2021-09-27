6aa7ffbf3528ce7b8b6575da7802487b2ec6ce8c
Peter Hardy <peter@phozart.com>
2021-09-27 21:19:37 +0200

    addContactForm

--- portfolio.js
+++ portfolio.js
@@ -53,7 +53,7 @@ function navigate(tab) {
     case "Projects":
         loadProjects();
         break;
-    case "@":
+    case "Contact":
         loadContact();
     default:
         loadHome();
@@ -146,7 +146,29 @@ function loadContact() {
     
     right.innerHTML = "";
     right.innerHTML += "<div>Contact form come here</div";
-
+    //following contact form is copied from https://mdbootstrap.com/docs/b4/jquery/forms/contact/#html
+    right.innerHTML += '<section class="mb-4">'
+        + '<h2 class="h1-responsive font-weight-bold text-center my-4">Contact me</h2>'
+        + '<div class="row"> <div class="col-md-9 mb-md-0 mb-5">'
+        +  '<form id="contact-form" name="contact-form" action="mail.php" method="POST">'
+        +  '<div class="row"><div class="col-md-6"><div class="md-form mb-0">'
+        +  '<input type="text" id="name" name="name" class="form-control">'
+        +  '<label for="name" class="">Your name</label></div>'
+        +  '</div><div class="col-md-6"><div class="md-form mb-0">'
+        +   '<input type="text" id="email" name="email" class="form-control">'
+        +   '<label for="email" class="">Your email</label></div></div></div>'
+        +   '<div class="row"><div class="col-md-12"><div class="md-form mb-0">'
+        +  '<input type="text" id="subject" name="subject" class="form-control">'
+        +   '<label for="subject" class="">Subject</label></div></div></div>'
+        +  '<div class="row"><div class="col-md-12"><div class="md-form">'
+        +  '<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>'
+        +  '<label for="message">Your message</label> </div></div></div></form>'
+        +  '<div class="text-center text-md-left">'
+        +  '<a class="btn btn-primary" onclick="document.getElementById(&apos;contact-form&apos;).submit();">Send</a>'
+        +  '</div><div class="status"></div> </div><div class="col-md-3 text-center">'
+        +  '<ul class="list-unstyled mb-0"><li><i class="fas fa-map-marker-alt fa-2x"></i><p>Belgium</p></li>'
+        +  '<li><i class="fas fa-phone mt-4 fa-2x"></i><p>+ 32 493 63 01 15</p></li>'
+        +  '<li><i class="fas fa-envelope mt-4 fa-2x"></i><p>peter.hardy@ouiquic.com</p></li></ul></div></div></section></div>'
 
 
 }
